8ccc56c79f19c9ca8d70065b63610e097d53706c
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-12-06 15:00:02 -0500

    Update environment variable

--- client/src/API/categoryCall.js
+++ client/src/API/categoryCall.js
@@ -1,39 +1,41 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/categories";
+const BASE_URL =
+  `${process.env.DATABASE_URL}/api/categories` ||
+  "http://localhost:3000/api/categories";
 
 export const getAllCategories = async () => {
-    try {
-        const { data } = await axios.get(BASE_URL);
-        return data;
-    } catch (error) {
-        console.error("Error fetching categories:", error);
-    }
-}
+  try {
+    const { data } = await axios.get(BASE_URL);
+    return data;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+};
 
 export const getCategoryById = async (id) => {
-    try {
-        const { data } = await axios.get(`${BASE_URL}/${id}`);
-        return data;
-    } catch (error) {
-        console.error("Error fetching category:", error);
-    }
-}
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching category:", error);
+  }
+};
 
 export const createCategory = async (name) => {
-    try {
-        const { data } = await axios.post(`${BASE_URL}/create`, { name });
-        return data;
-    } catch (error) {
-        console.error("Error creating category:", error);
-    }
-}
+  try {
+    const { data } = await axios.post(`${BASE_URL}/create`, { name });
+    return data;
+  } catch (error) {
+    console.error("Error creating category:", error);
+  }
+};
 
 export const deleteCategory = async (id) => {
-    try {
-        const { data } = await axios.delete(`${BASE_URL}/delete/${id}`);
-        return data;
-    } catch (error) {
-        console.error("Error deleting category:", error);
-    }
-}
+  try {
+    const { data } = await axios.delete(`${BASE_URL}/delete/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error deleting category:", error);
+  }
+};
--- client/src/API/itemCall.js
+++ client/src/API/itemCall.js
@@ -1,48 +1,63 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/items";
+const BASE_URL =
+  `${process.env.DATABASE_URL}/api/items` || "http://localhost:3000/api/items";
 
 export const getAllItems = async () => {
-    try {
-        const { data } = await axios.get(BASE_URL);
-        return data;
-    } catch (error) {
-        console.error("Error fetching items:", error);
-    }
-}
+  try {
+    const { data } = await axios.get(BASE_URL);
+    return data;
+  } catch (error) {
+    console.error("Error fetching items:", error);
+  }
+};
 
 export const getItemById = async (id) => {
-    try {
-        const { data } = await axios.get(`${BASE_URL}/${id}`);
-        return data;
-    } catch (error) {
-        console.error("Error fetching item:", error);
-    }
-}
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching item:", error);
+  }
+};
 
 export const getItemsByCategory = async (id) => {
-    try {
-        const { data } = await axios.get(`${BASE_URL}/category/${id}`);
-        return data;
-    } catch (error) {
-        console.error("Error fetching items by category:", error);
-    }
-}
+  try {
+    const { data } = await axios.get(`${BASE_URL}/category/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching items by category:", error);
+  }
+};
 
-export const createItem = async (name, price, category_id, type, expiry_date, date_created) => {
-    try {
-        const { data } = await axios.post(`${BASE_URL}/create`, { name, price, category_id, type, expiry_date, date_created });
-        return data;
-    } catch (error) {
-        console.error("Error creating item:", error);
-    }
-}
+export const createItem = async (
+  name,
+  price,
+  category_id,
+  type,
+  expiry_date,
+  date_created
+) => {
+  try {
+    const { data } = await axios.post(`${BASE_URL}/create`, {
+      name,
+      price,
+      category_id,
+      type,
+      expiry_date,
+      date_created,
+    });
+    return data;
+  } catch (error) {
+    console.error("Error creating item:", error);
+  }
+};
 
 export const deleteItem = async (id) => {
-    try {
-        const { data } = await axios.delete(`${BASE_URL}/delete/${id}`);
-        return data;
-    } catch (error) {
-        console.error("Error deleting item:", error);
-    }
-}
+  try {
+    const { data } = await axios.delete(`${BASE_URL}/delete/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error deleting item:", error);
+  }
+};
